8e14d57a714ad4955492e61ca72e20aabb83dfa9
Brindi cram <neilj0913@gmail.com>
2026-06-20 08:02:18 +0100

    Update author and description in settings.js

--- settings.js
+++ settings.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 const settings = {
   packname: process.env.PACK_NAME || 'BRINDI-XMD',
-  author: process.env.PACK_AUTHOR || 'BRANDON',
+  author: process.env.PACK_AUTHOR || 'B.r.a.n_don14✨',
   botName: process.env.BOT_NAME || 'BRINDI-XMD',
-  botOwner: process.env.OWNER_NAME || 'Brindi Cram',
+  botOwner: process.env.OWNER_NAME || 'B.r.a.n_don14✨',
   ownerNumber: process.env.OWNER_NUMBER || '237673355468',
   prefix: process.env.PREFIX || '.',
   giphyApiKey: process.env.GIPHY_API_KEY || '',// votre api giphy
   commandMode: process.env.COMMAND_MODE || 'private',
   maxStoreMessages: 20,
   storeWriteInterval: 10000,
-  description: "Bot WhatsApp multifonctions - Brindi-XMD",
+  description: "Bot WhatsApp Xerone multifonctions - Brindi-XMD",
   version: process.env.BOT_VERSION || '1.0.0',
   ytch: process.env.YT_CHANNEL || '',
   waChannel: process.env.WA_CHANNEL || '',
